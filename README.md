6dba977d6e8e0dd0cbfd77ec7e0525a95cea9eeb
James Thistlewood <jamesthistlewood@gmail.com>
2020-05-10 00:02:51 +0100

    Add config file, proper handling of old data, versioning system, and more

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 ## Installation
 
-1. `yarn`
+`yarn`
+
+## Config
+
+`cp config.example.json config.json`
+
+Then adjust it to suit your needs.
+
+Notes on config fields:
+
+- `host`: the database host. Should include username and password in `uname:pwd@example.com` format.
+- `database`: the Mongo database to use
+- `datasets`: an array of objects with the form:
+  - `id`: a unique identifier for this dataset, which must be kept the same if you want any changes to _update_ the database rather than simply append to it.
+  - `src`: the location of the source GeoJSON file
+  - `version`: the version of this dataset. Increment this whenever the source file or anything to do with this dataset changes.
+  - `priority`: if a loo lands within multiple areas, the one with the highest priority will be the one to which it is assigned.
+  - `type`: the name of this type of area, e.g. 'Local Authority'
 
 ## Usage
 
@@ -15,19 +32,12 @@ or
 `./index.js [options]`
 
 ```txt
-usage: index.js [-h] [-v] [-a HOST] [-d DATABASE] [-n] boundaries
+usage: index.js [-h] [-v] [-n]
 
 Update the database with areas from ONS GeoJSON
 
-Positional arguments:
-  boundaries            The ONS area boundaries GeoJSON file
-
 Optional arguments:
-  -h, --help            Show this help message and exit.
-  -v, --version         Show program's version number and exit.
-  -a HOST, --host HOST  The database host
-  -d DATABASE, --database DATABASE
-                        The database name
-  -n, --dry-run         Don't update the database, just output what would be
-                        done.
+  -h, --help     Show this help message and exit.
+  -v, --version  Show program's version number and exit.
+  -n, --dry-run  Don't update the database, just output what would be done.
 ```
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('dotenv').config()
+const config = require('./config.json');
 const fs = require('fs');
 const mongoose = require('mongoose');
 require('./setup');
@@ -13,20 +13,6 @@ const parser = new ArgumentParser({
   description: 'Update the database with areas from ONS GeoJSON'
 });
 
-parser.addArgument('boundaries', {
-  help: 'The ONS area boundaries GeoJSON file'
-});
-
-parser.addArgument(['-a', '--host'], {
-  help: 'The database host',
-  required: false,
-});
-
-parser.addArgument(['-d', '--database'], {
-  help: 'The database name',
-  required: false,
-});
-
 parser.addArgument(['-n', '--dry-run'], {
   help: 'Don\'t update the database, just output what would be done.',
   action: 'storeConst',
@@ -37,6 +23,9 @@ parser.addArgument(['-n', '--dry-run'], {
 
 const Area = mongoose.model('Area');
 
+/**
+ * Loads a json file which doesn't have .json as a file extension.
+ */
 function loadJSON(filename) {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, 'utf-8', (error, res) => {
@@ -49,19 +38,132 @@ function loadJSON(filename) {
   });
 }
 
+/**
+ * Do any cleanup and exit the programme.
+ */
 function finish() {
   console.log('Exiting...');
   process.exit(0);
 }
 
+/**
+ * Mmmm, curry.
+ */
+function datasetLogger(dataset) {
+  return (...args) => {
+    console.log(`${dataset.id}:`, ...args);
+  }
+}
+
+/**
+ * Update the database from a dataset-format object.
+ */
+function updateDataset(dataset, dryrun) {
+  return new Promise(async (doneUpdating) => {
+    const log = datasetLogger(dataset);
+
+    log(`Loading GeoJSON file ${dataset.src}...`);
+    const bounds = await loadJSON(dataset.src);
+    log(`Loaded: ${bounds.name}`);
+
+    // Do some preliminary checks
+    if (dryrun) {
+      log(`Would delete old areas with id==${dataset.id}, version!=${dataset.version}`);
+    } else {
+      log('Searching for non-stale area data in database...');
+      const nonStale = await new Promise((resolve) => {
+        Area.findOne({
+          version: {
+            $eq: dataset.version,
+          }
+        }, (err, res) => {
+          return resolve(res);
+        });
+      });
+
+      if (nonStale != null) {
+        log(`WARNING: Type ${dataset.type} has non-stale entries in the database. This means you probably need to increment the version number, not changing anything for this dataset.`);
+        return doneUpdating(false);
+      }
+
+      // Get rid of stale data
+      log('Deleting stale data...');
+      await new Promise((resolve) =>
+        Area.deleteMany({
+          datasetId: {
+            $eq: dataset.id,
+          }
+        }, (err, res) => {
+          if (err) {
+            log('Error deleting:', err);
+            return doneUpdating(false);
+          }
+          return resolve();
+        })
+      );
+      log('Deleted successfully, now writing new area data...');
+    }
+
+    // Actually write the areas to the database
+    let count = bounds.features.length;
+    let saved = 0;
+    bounds.features.forEach((feature) => {
+      const name = feature.properties.lad19nm;
+      const geometry = feature.geometry;
+
+      const newArea = new Area();
+      newArea.name = name;
+      newArea.geometry = geometry;
+      newArea.datasetId = dataset.id;
+      newArea.priority = dataset.priority;
+      newArea.version = dataset.version;
+      newArea.type = dataset.type;
+
+      if (dryrun) {
+        log(`Would save area ${name}`);
+      } else {
+        newArea.save((err) => {
+          if (err) {
+            console.error(`${dataset.id}: Error saving area: ${err}`);
+            process.exit(1);
+          }
+          saved += 1;
+          if (saved === count) {
+            log('Successfully written new areas for this dataset');
+            return doneUpdating(true);
+          }
+        });
+      }
+    });
+  });
+}
+
+function sanityCheck(datasets) {
+  const ids = [];
+
+  datasets.forEach((dataset) => {
+    if (ids.includes(dataset.id)) {
+      console.log('Config is insane! You cannot have two datasets with the same id.');
+      return false;
+    }
+    ids.push(dataset.id);
+  });
+  return true;
+}
+
+/**
+ * Entry point.
+ */
 async function run() {
   const args = parser.parseArgs();
   const dryrun = args.dry_run;
 
-  const host = args.host || process.env.HOST;
-  const database = args.database || process.env.DATABASE;
+  if (!sanityCheck(config.datasets)) {
+    console.log('Sanity check failed');
+    return finish();
+  }
 
-  const dbAddr = `mongodb://${host}/${database}`;
+  const dbAddr = `mongodb://${config.host}/${config.database}`;
   if (!dryrun) {
     console.log(`Connecting to ${dbAddr}`);
     await mongoose.connect(dbAddr, {
@@ -70,45 +172,23 @@ async function run() {
     });
   }
 
-  const filename = args.boundaries;
-
-  console.log('Loading GeoJSON file...');
-  const bounds = await loadJSON(filename);
-  console.log(`Loaded: ${bounds.name}`);
-
-  let count = 0;
+  let count = config.datasets.length;
   let saved = 0;
-  bounds.features.forEach((feature) => {
-    const name = feature.properties.lad19nm;
-    const geometry = feature.geometry;
-
-    const newArea = new Area();
-    newArea.name = name;
-    newArea.geometry = geometry;
-
-    if (dryrun) {
-      console.log(`Would save area ${name}`);
-    } else {
-      newArea.save((err) => {
-        if (err) {
-          console.error(`Error saving area: ${err}`);
-          process.exit(1);
-        }
-        saved += 1;
-        if (saved == count) {
-          console.log('Saved all areas');
-          finish();
-        }
-      });
+  let success = 0;
+  config.datasets.forEach(async (dataset) => {
+    const res = await updateDataset(dataset, dryrun);
+    saved += 1;
+    success += res ? 1 : 0;
+    if (saved === count) {
+      console.log(`Saved all saveable areas, ${success} successes`);
+      finish();
     }
-    count += 1;
   });
 
   if (dryrun) {
     console.log(`Would have added ${count} areas`);
-    finish();
   } else {
-    console.log(`Parsed ${count} areas, saving...`);
+    console.log(`Parsed ${count} areas, please wait while saving...`);
   }
 }
 
